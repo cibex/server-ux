6c2bbc87013e89fe46491abb33084d7a4e361b22
okt <o.ekici@cibex.net>
2022-08-25 09:47:08 +0200

    Add model _description
    Signed-off-by: okt <o.ekici@cibex.net>

--- date_range/tests/models.py
+++ date_range/tests/models.py
@@ -4,6 +4,7 @@ from odoo import fields, models
 class TestDateRangeSearchMixin(models.Model):
     _name = "test.date.range.search.mixin"
     _inherit = ["date.range.search.mixin"]
+    _description = "Test Mixin class to add a Many2one style period search field"
     _date_range_search_field = "test_date"
 
     name = fields.Char()
